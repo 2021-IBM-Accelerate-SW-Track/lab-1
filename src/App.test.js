458354c4bee78e6ae573873632872e6ce53f3170
Alexandra Isaly <alexandraisaly@gmail.com>
2021-06-08 10:00:29 -0400

    add testing for testids

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { render, cleanup, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+afterEach(cleanup)
+
+test('test that App component renders', () => {
   render(<App />);
-  const linkElement = screen.getByText(/2021 IBM Accelerate/i);
-  expect(linkElement).toBeInTheDocument();
  });
+
+test('test that new-item-button is a button', () => {
+  render(<App/>);
+  const element = screen.getByTestId('new-item-button');
+  expect(element.innerHTML.toLowerCase().includes("button")).toBe(true)
+});
+
+test('test that new-item-input is an input ', () => {
+  render(<App/>);
+  const element = screen.getByTestId('new-item-input');
+  expect(element.innerHTML.toLowerCase().includes("input")).toBe(true)
+});
