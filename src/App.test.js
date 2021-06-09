dd084b55c2bf074e125e08adecf10d0c3eae1cc9
Alexandra Isaly <alexandraisaly@gmail.com>
2021-06-09 17:55:20 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,33 @@
-import { render, cleanup, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { unmountComponentAtNode } from 'react-dom';
 import App from './App';
 
-afterEach(cleanup)
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
 
 test('test that App component renders', () => {
-  render(<App />);
+  render(<App />, container);
  });
 
 test('test that new-item-button is a button', () => {
-  render(<App/>);
+  render(<App/>, container);
   const element = screen.getByTestId('new-item-button');
   expect(element.innerHTML.toLowerCase().includes("button")).toBe(true)
 });
 
 test('test that new-item-input is an input ', () => {
-  render(<App/>);
+  render(<App/>, container);
   const element = screen.getByTestId('new-item-input');
   expect(element.innerHTML.toLowerCase().includes("input")).toBe(true)
 });
